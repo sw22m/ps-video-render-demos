2290a6bccbe63e62b218bab0a939efb27ffb0d0b
sw22m <142818407+sw22m@users.noreply.github.com>
2023-12-15 14:43:00 +0000

    Update rtsp_test_controls.py

--- python-examples/QGstVideoRender/examples/rtsp_test_controls.py
+++ python-examples/QGstVideoRender/examples/rtsp_test_controls.py
@@ -25,16 +25,18 @@ Gst.init(None)
 
 class ARtspMediaFactory(GstRtspServer.RTSPMediaFactory):
 
-    def __init__(self, pipeline, *args, **kwargs):
+    def __init__(self, port, host="localhost", pipeline=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.pipeline = pipeline
+        self.port = port
+        self.host = host
 
     def do_create_element(self, url):
         video_src = 'videotestsrc'
         rtsp_bin = Gst.Bin.new("rtsp_bin")
         udp_src = Gst.ElementFactory.make("udpsrc")
         udp_src.set_property("name", "pay0")
-        udp_src.set_property("port", 8004)
+        udp_src.set_property("port", self.port)
 
         caps = Gst.Caps.new_empty_simple("application/x-rtp")
         caps.set_value("media", "video")
@@ -43,12 +45,14 @@ class ARtspMediaFactory(GstRtspServer.RTSPMediaFactory):
         caps.set_value("encoding-name", "H264")
         caps.set_value("payload", 96)
         udp_src.set_property("caps", caps)
-        print(self.pipeline)
         rtsp_bin.add(udp_src)
         return rtsp_bin
 
 
 class VideoWindow(QMainWindow):
+
+    RTSP_SERVER_HOST = "127.0.0.1"
+    RTSP_SERVER_PORT = 8004
 
     def __init__(self, parent=None):
         super(VideoWindow, self).__init__(parent)
@@ -71,8 +75,7 @@ class VideoWindow(QMainWindow):
         openh264enc = Gst.ElementFactory.make("openh264enc")
         h264parse = Gst.ElementFactory.make("h264parse")
 
-        RTSP_SERVER_HOST = "127.0.0.1"
-        RTSP_SERVER_PORT = 8004
+
 
         rtph264_filter = Gst.ElementFactory.make("rtph264pay")
         rtph264_filter.set_property("name", "pay0")
@@ -99,7 +102,7 @@ class VideoWindow(QMainWindow):
 
         udp_sink = Gst.ElementFactory.make("udpsink")
         udp_sink.set_property("host", "127.0.0.1")
-        udp_sink.set_property("port", RTSP_SERVER_PORT)
+        udp_sink.set_property("port", self.RTSP_SERVER_PORT)
         self.player.add(udp_sink)
         rtph264_filter.link(udp_sink)
 
@@ -107,28 +110,26 @@ class VideoWindow(QMainWindow):
         video_src = 'videotestsrc'
 
         # Create RTSP server
-        server = GstRtspServer.RTSPServer.new()
-        server.props.service = f"{RTSP_SERVER_PORT}"
-        server.attach(None)
-        mount_point = "feed"
+        self.server = GstRtspServer.RTSPServer.new()
+        self.server.props.service = f"{self.RTSP_SERVER_PORT}"
+        self.server.attach(None)
+        self.mount_point = "feed"
 
         # media_factory = ARtspMediaFactory.custom_pipeline_factory(self.player)
-        media_factory = ARtspMediaFactory(rtph264_filter)
+        media_factory = ARtspMediaFactory(port=self.RTSP_SERVER_PORT)
         media_factory.set_shared(True)
         # rtsp_url = GstRtsp.RTSPUrl()
         # rtsp_url.set_port(8554)
         self.player.set_state(Gst.State.PLAYING)
-        server.get_mount_points().add_factory(f"/{mount_point}", media_factory)
-        server.attach(None)
+        self.server.get_mount_points().add_factory(f"/{self.mount_point}", media_factory)
+        self.server.attach(None)
 
-        url = f"rtsp://127.0.0.1:{RTSP_SERVER_PORT}/{mount_point}"
+        url = f"rtsp://127.0.0.1:{self.RTSP_SERVER_PORT}/{self.mount_point}"
         self.textoverlay.set_property("text", f"{url}")
         self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         video_widget = QVideoWidget()
-        w.layout().addWidget(video_widget)
         self.player.setVideoOutput(video_widget)
-        self.player.setMedia(QMediaContent(QUrl(url)))
-        self.player.play()
+        w.layout().addWidget(video_widget)
 
         self.pb_start = QPushButton("Start Server")
         self.pb_start.setCheckable(True)
@@ -137,6 +138,17 @@ class VideoWindow(QMainWindow):
 
     def on_start(self):
         print(f"Enable server: {self.pb_start.isChecked()}")
+        if self.pb_start.isChecked():
+            self.play()
+
+    def play(self):
+        url = f"rtsp://127.0.0.1:{self.RTSP_SERVER_PORT}/{self.mount_point}"
+        self.textoverlay.set_property("text", f"{url}")
+        self.player.setMedia(QMediaContent(QUrl(url)))
+        self.player.play()
+
+    def stop(self):
+        print("Stop server")
 
 
 if __name__ == '__main__':
